dc18b721f5629d8afd1a4119bca06e252ec1b516
geemoowy <kovylek.ul@gmail.com>
2025-03-25 12:33:30 +0300

    refactoring views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from catalog.models import Product
+
 
 def home(request):
+    latest_products = Product.objects.order_by('-created_at')[:5]
+
+    for product in latest_products:
+        print(f'Продукт: {product.name}, Цена: {product.purchase_price}, Дата создания: {product.created_at}')
+
     return render(request, 'home.html')
 
 
