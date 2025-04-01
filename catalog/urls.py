516e8f880cd31d13d84f36b378d11f12c4c27784
geemoowy <kovylek.ul@gmail.com>
2025-04-01 23:33:23 +0300

    add forms.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from .views import home, contacts, product_detail
+from .views import home, contacts, product_detail, add_product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('product_detail/<int:id>/', product_detail, name='product_detail')  # Изменено
+    path('product_detail/<int:id>/', product_detail, name='product_detail'),
+    path('add_product/', add_product, name='add_product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 from catalog.models import Product
+from .forms import ProductForm
 
 
 def home(request):
-    products = Product.objects.all()
+    products_list = Product.objects.all()
+    paginator = Paginator(products_list, 6)
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
+
     return render(request, 'home.html', {'products': products})
 
 
@@ -21,3 +27,14 @@ def contacts(request):
 def product_detail(request, id):
     product = get_object_or_404(Product, id=id)  # Получаем продукт по ID
     return render(request, 'product_detail.html', {'product': product})
+
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:home')
+    else:
+        form = ProductForm
+    return render(request, 'add_product.html', {'form': form})
