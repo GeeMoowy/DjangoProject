e079b0439449d098e6f774719109bb2ca8d3219e
geemoowy <kovylek.ul@gmail.com>
2025-04-10 17:50:04 +0300

    add send email

--- blogging/views.py
+++ blogging/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from django.views.generic.edit import CreateView
+from django.core.mail import send_mail
+from django.conf import settings
 
 from blogging.forms import BlogForm
 from blogging.models import Blog
@@ -40,7 +42,19 @@ class BlogDetailView(DetailView):
         self.object = super().get_object(queryset)
         self.object.views_count += 1
         self.object.save()
+
+        if self.object.views_count == 100:
+            self.send_notification()
+
         return self.object
+
+    def send_notification(self):
+        subject = 'Блог достиг 100 просмотров!'
+        message = f'Ваш блог "{self.object.title}" достиг 100 просмотров.'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ['kovylek.ul@gmail.com']
+
+        send_mail(subject, message, email_from, recipient_list)
 
 
 class BlogDeleteViews(DeleteView):
--- config/settings.py
+++ config/settings.py
@@ -98,3 +98,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kovylek.ul@mail.ru'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
